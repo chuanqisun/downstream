737838d0c9326a624857db5f6e7ebcb323b592a0
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-06-09 20:53:42 -0700

    split site from lib

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "Semidown",
+      fileName: "semidown",
+    },
+    sourcemap: "inline",
+  },
+});
